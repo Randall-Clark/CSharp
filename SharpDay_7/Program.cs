d5bfee5a1f2991eedc17b330616d6843121d8171
Randall-Clark <clark.tom-ovolo@epitech.eu>
2025-04-08 11:45:14 -0400

    Day 7 Basic Math computing algorithm

---
+++ SharpDay_7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * ---
+ * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+ **/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number one value:");
+        
+        int value1 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Enter the number two value:");
+        
+        int value2 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"{value1} + {value2} = {value1 + value2}");
+        Console.WriteLine($"{value1} - {value2} = {value1 - value2}");
+        Console.WriteLine($"{value1} * {value2} = {value1 * value2}");
+        Console.WriteLine($"{value1} / {value2} = {value1 / value2}");
+        Console.WriteLine($"{value1} mod {value2} = {value1 % value2}");
+        
+    }
+}
---
+++ SharpDay_7/README.md
@@ -0,0 +1,12 @@
+# Day 7 Basics Calculator Program
+
+## Objectives
+Completea program that take two values as input and return the differents result of the following computings process on the output
+
+- Addition
+- Substraction
+- Multiplication
+- Division
+- Modulo
+
+NB: This version is coded without the handling of the differents mathematical errors exception such as division by 0 who is know to be impossible or more an error than anything else.
