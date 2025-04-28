125cee8e65346df0772b9a6978acc85e04161d1c
Randall TOM'OVOLO <clark.tom-ovolo@epitech.eu>
2025-04-28 17:20:07 -0400

    Set Bases Learning

---
+++ SharpDay_16/Program.cs
@@ -0,0 +1,19 @@
+﻿ISet<string> ingredients = new HashSet<string>();
+IList<string> ingredientsList = new List<string>();
+ingredients.Add("sucre");
+ingredients.Add("chocolat");
+ingredients.Add("beurre");
+ingredients.Add("vanille");
+
+// TODO : ajouter un autre ingrédient à l'ensemble
+ingredients.Add("milk");
+
+// TODO : retirer la vanille de l'ensemble
+ingredients.Remove("vanille");
+
+// Afficher la liste des ingrédients
+Console.WriteLine("Voici la liste de nos ingrédients");
+foreach (string ingredient in ingredients)
+{
+    Console.WriteLine(ingredient);
+}
